04e525271c07b1b34bdb5700c06c8affef4ba713
mikhalich2701 <38112568+mikhalich2701@users.noreply.github.com>
2018-09-10 22:25:15 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	$('.fast_food_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+	$('.drinks_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+	$('.chicken_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+	$('.rice_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+	$('.customer_slide').slick({
+		centerMode: true,
+   		centerPadding: '0px',
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+});
